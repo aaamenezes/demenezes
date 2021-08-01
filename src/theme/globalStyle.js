f52ee87f8400e0a168821f48a9a8b097a5f7b5cf
andre-menezes <andre.menezes@vnda.com.br>
2021-08-01 13:51:06 -0300

    Criar estilos iniciais do blog

---
+++ src/theme/globalStyle.js
@@ -0,0 +1,90 @@
+import { createGlobalStyle } from 'styled-components'
+import { normalize } from 'styled-normalize'
+
+export const GlobalStyle = createGlobalStyle`
+
+  ${ normalize }
+
+  * {
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+  }
+
+  body {
+    ${ ({ theme }) => ({
+      fontSize: theme.fontSize.base.desktop,
+      fontFamily: theme.fontFamily.text,
+      lineHeight: 1.5,
+      color: theme.color.black,
+      backgroundColor: theme.color.white,
+    })}
+  }
+
+  /* #__next {
+    min-height: 100vh;
+  } */
+
+  h1 {
+    ${ ({ theme }) => ({
+      fontSize: theme.fontSize.pageTitle,
+      fontFamily: theme.fontFamily.title,
+    })}
+  }
+
+  h2 {
+    ${ ({ theme }) => ({
+      fontSize: theme.fontSize.sectionTitle,
+      fontFamily: theme.fontFamily.title,
+    })}
+  }
+
+  h3 {
+    ${ ({ theme }) => ({
+      fontSize: theme.fontSize.articleTitle,
+      fontFamily: theme.fontFamily.title,
+    })}
+  }
+
+  p {
+    ${ ({ theme }) => ({
+      fontSize: theme.fontSize.text,
+    })}
+  }
+
+  ul {
+    ${ ({ theme }) => ({
+      fontSize: theme.fontSize.text,
+    })}
+  }
+
+  li {
+    ${ ({ theme }) => ({
+      fontSize: theme.fontSize.text,
+    })}
+  }
+
+  span {
+    ${ ({ theme }) => ({
+      fontSize: theme.fontSize.text,
+    })}
+  }
+
+  a {
+    ${ ({ theme }) => ({
+      fontSize: theme.fontSize.text,
+    })}
+  }
+
+  img {
+    display: block;
+    max-width: 100%;
+    margin: 0 auto;
+  }
+
+  button {
+    border: none;
+    outline: none;
+    background: transparent;
+  }
+`
---
+++ src/theme/index.js
@@ -0,0 +1,137 @@
+const color = {
+  red1: '#D10015',
+  red2: '#C40014',
+  red3: '#AB0011',
+  red4: '#91000F',
+  red5: '#85000D',
+  redAlt1: '#fc1f0f',
+  redAlt2: '#d11a0d',
+  redAlt3: '#c4170b',
+  redAlt4: '#bd170b',
+  redAlt5: '#b0140a',
+  redAlt6: '#ab150a',
+  redAlt7: '#961209',
+  redAlt8: '#911209',
+  redAlt9: '#7d0f07',
+  white: '#FFFFFF',
+  gray1: '#FAFAFA',
+  gray2: '#EEEEEE',
+  gray3: '#DDDDDD',
+  gray4: '#D4D4D4',
+  gray5: '#D0D0D0',
+  gray6: '#CCCCCC',
+  gray7: '#BBBBBB',
+  gray8: '#AAAAAA',
+  black: '#121221',
+  blackAlt: '#333333',
+}
+
+const borderRadius = '4px'
+
+const boxShadow = {
+  low: '0 0 40px -30px black',
+  medium: '0 0 40px -20px black',
+  high: '0 0 40px -10px black'
+}
+
+const breakpoints = {
+  // xs: '0',
+  sm: '576',
+  md: '768',
+  lg: '992',
+  xl: '1200',
+  xxl: '1400',
+}
+
+const fontFamily = {
+  title: 'Ubuntu, sans-serif',
+  text: '"Montserrat", sans-serif',
+  code: 'Ubuntu Mono, sans-serif',
+  // Fontes reservas caso dê ruim
+  // fontFamily: 'Raleway, sans-serif',
+  // fontFamily: 'Open Sans, sans-serif',
+}
+
+/**
+ * Fator de escala: 1500
+ * Link do type-scale com a configuração atual
+ * https://type-scale.com/?size=20&scale=1.500&text=A%20Visual%20Type%20Scale&font=Montserrat&fontweight=400&bodyfont=body_font_default&bodyfontweight=400&lineheight=1.75&backgroundcolor=%23ffffff&fontcolor=%23000000&preview=false
+*/
+const fontSize = {
+  pageTitle: '3.375rem', // h1
+  sectionTitle: '2.25rem', // h2
+  articleText: '1.5rem', // h3, button
+  text: '1rem', // p, a, span
+  subText: '.667rem',
+  subSubText: '.444rem',
+  base: {
+    desktop: '20px',
+    mobile: '16px'
+  }
+}
+
+const fontWeight = {
+  regular: '400', // text
+  semiBold: '600', // h2, h3, button
+  bold: '700' // h1
+}
+
+const gutter = (...args) => {
+  const multiplier = 4
+  const validValues = args.slice(0, 4)
+  const valuesWithPX = validValues.map(value => `${value * multiplier}px`)
+  const values = valuesWithPX.join().replaceAll(',', ' ')
+  return values
+}
+
+const letterSpacing = {
+  // ainda não sei quais são os valores
+  // title: ,
+  // text: 
+}
+
+const lineHeight = {
+  title: 1.7,
+  text: 1.2
+}
+
+const opacity = {
+  transparent: '.3',
+  medium: '.6',
+  light: '.9'
+}
+
+const transition = {
+  slow: '1.3s',
+  medium: '.8s',
+  fast: '.3s'
+}
+
+const zIndex = {
+  under: -1,
+  relative: 900,
+  dropdown: 1000,
+  absolute: 1010,
+  sticky: 1020,
+  fixed: 1030,
+  modalBackdrop: 1040,
+  modal: 1050,
+  popover: 1060,
+  tooltip: 1070
+}
+
+export const theme = {
+  color,
+  borderRadius,
+  boxShadow,
+  breakpoints,
+  fontFamily,
+  fontSize,
+  fontWeight,
+  gutter,
+  letterSpacing,
+  lineHeight,
+  opacity,
+  transition,
+  zIndex,
+}
