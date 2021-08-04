61a4c1d3b332cafd7a93d91c5e262d68dab5b3c4
andre-menezes <andre.menezes@vnda.com.br>
2021-08-03 21:05:50 -0300

    Importar icones fontawesome

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,18 @@
 import React from 'react'
 import { ThemeProvider } from 'styled-components'
+import Head from 'next/head'
 import { theme } from '../src/theme'
 import { GlobalStyle } from '../src/theme/globalStyle'
 
 export default function MyApp({ Component, pageProps }) {
   return (
     <>
-      {/* <Head>
-
-      </Head> */}
+      <Head>
+        <link
+          rel='stylesheet'
+          href='https://use.fontawesome.com/releases/v5.15.2/css/all.css'
+        />
+      </Head>
       <ThemeProvider theme={theme}>
         <GlobalStyle />
         <Component {...pageProps} />
