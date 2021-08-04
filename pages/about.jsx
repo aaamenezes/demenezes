4528c0ff79af98183bad426171e984d55aa3a9ae
andre-menezes <andre.menezes@vnda.com.br>
2021-08-03 21:05:06 -0300

    Criar pagina about

---
+++ pages/about.jsx
@@ -0,0 +1,14 @@
+// Referência de conteúdo pro texto
+// https://emersonbroga.com/e/sobre/
+
+import React from 'react'
+import Header from '../src/components/common/Header'
+
+export default function About() {
+  return (
+    <>
+      <Header />
+      <div>otariu</div>
+    </>
+  )
+}
