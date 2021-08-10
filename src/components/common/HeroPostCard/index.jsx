a79caabf23f19cddaea61f53818dcbf26b6bed80
andre-menezes <andre.menezes@vnda.com.br>
2021-08-09 22:47:45 -0300

    Isolar componente HeroPost

---
+++ src/components/common/HeroPostCard/index.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import styled, { css } from 'styled-components'
+import Container from '../Container'
+
+const HeroWrapper = styled.article`
+  height: 100vh;
+  padding-top: 74px;
+  background: ${ ({ theme, post }) => css`linear-gradient(
+    0deg,
+    ${ theme.color.black + theme.opacity.medium.hex },
+    ${ theme.color.black + theme.opacity.medium.hex }
+    ), url('${ post.coverImage }')
+  ` };
+  background-repeat: no-repeat;
+  background-position: center;
+  background-size: cover;
+  background-attachment: fixed;
+`
+
+const HeroTitle = styled.h3`
+  color: ${ ({ theme }) => theme.color.white };
+`
+
+export default function HeroPostCard({ post }) {
+  return (
+    <Container as={HeroWrapper} post={post}>
+      <HeroTitle>{post.title}</HeroTitle>
+    </Container>
+  )
+}
