fd2a36c09f8ff2841ed982cf450cadb7977e675c
andre-menezes <andre.menezes@vnda.com.br>
2021-08-16 21:50:27 -0300

    Criar seção de newsletter WIP (falta criar email próprio e conta no MailChimp)

---
+++ src/components/HomePage/PostsWrapper/index.jsx
@@ -0,0 +1,57 @@
+import React from 'react'
+import styled from 'styled-components'
+import HeroPostCard from '../HeroPostCard'
+import PostCard from '../../common/PostCard'
+import Newsletter from '../../common/Newsletter'
+import { getGutter } from '../../../utils/getGutter'
+
+const StyledPostsWrapper = styled.main`
+  > * {
+    margin-bottom: ${ getGutter('15') };
+  }
+`
+
+export default function PostsWrapper({ postsList }) {
+  const heroPost = postsList[0]
+
+  const firstBlockStart = 1
+  const firstBlockEnd = postsList.length / 2
+  const firstBlockPosts = postsList.slice(firstBlockStart, firstBlockEnd).map(
+    (post, index) => (
+      <PostCard
+        key={post.title}
+        title={post.title}
+        postLink={`/posts/${ post.slug }`}
+        imageURL={post.coverImage}
+        imageAlt={`Imagem de capa do post: ${ post.title }`}
+        imageRatio='4x3'
+        index={index}
+      />
+    )
+  )
+
+  const secondBlockStart = firstBlockEnd
+  const secondBlockEnd = postsList.length
+  const secondBlockPosts = postsList.slice(
+    secondBlockStart, secondBlockEnd
+  ).map((post, index) => (
+    <PostCard
+      key={post.title}
+      title={post.title}
+      postLink={`/posts/${ post.slug }`}
+      imageURL={post.coverImage}
+      imageAlt={`Imagem de capa do post: ${ post.title }`}
+      imageRatio='4x3'
+      index={index}
+    />
+  ))
+
+  return (
+    <StyledPostsWrapper>
+      <HeroPostCard post={heroPost} />
+      {firstBlockPosts}
+      <Newsletter />
+      {secondBlockPosts}
+    </StyledPostsWrapper>
+  )
+}
---
+++ src/components/common/Newsletter/index.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import styled from 'styled-components'
+import { getGutter } from '../../../utils/getGutter'
+import Container from '../Container'
+
+const NewsletterWrapper = styled.section`
+  ${ ({ theme }) => ({
+    paddingTop: getGutter(5),
+    paddingBottom: getGutter(5),
+    color: theme.color.white,
+    backgroundColor: theme.color.gray
+  }) }
+`
+
+export default function Newsletter() {
+  return (
+    <Container as={NewsletterWrapper}>
+      <form>
+        {/* eslint-disable-next-line max-len */}
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Illum quos, culpa rerum odit eum labore asperiores voluptate delectus hic, nesciunt sint cumque suscipit, at vel aliquam magnam! Dolores, cum mollitia.</p>
+        <input
+          type='text'
+          value='name'
+          placeholder='Insira seu nome'
+          aria-label='Insira seu nome'
+        />
+        <input
+          type='email'
+          name='email'
+          placeholder='Insira seu e-mail'
+          aria-label='Insira seu e-mail'
+        />
+        <button type='submit'>
+          Quero me cadastrar
+        </button>
+        {/* eslint-disable-next-line max-len */}
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Illum quos, culpa rerum odit eum labore asperiores voluptate delectus hic, nesciunt sint cumque suscipit, at vel aliquam magnam! Dolores, cum mollitia.</p>
+      </form>
+    </Container>
+  )
+}
