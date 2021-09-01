2ffc3736dae8b6d93d6e305df25b59b1fc270eee
andre-menezes <andre.menezes@vnda.com.br>
2021-08-31 23:26:11 -0300

    Estilo de hover no título do postcard title

--- src/components/Common/PostCard/PostCardHeader.jsx
+++ src/components/Common/PostCard/PostCardHeader.jsx
@@ -4,7 +4,25 @@ import LinkButton from '../LinkButton'
 
 const PostCardHeaderWrapper = styled.header``
 const PostCardCategory = styled.p``
-const PostCardTitle = styled.h3``
+const PostCardTitle = styled.h3`
+  display: inline-block;
+  background-image: ${ ({ theme }) => (
+    `linear-gradient(
+      120deg,
+      ${ theme.color.red } 0%,
+      ${ theme.color.red5 } 100%
+    )`
+  ) };
+  background-repeat: no-repeat;
+  background-size: 100% 0.2em;
+  background-position: 0 108%;
+  transition: ${ ({ theme }) => theme.transition.fast };
+
+  &:hover {
+    color: ${ ({ theme }) => theme.color.white };
+    background-size: 100% 108%;
+  }
+`
 
 export default function PostCardHeader({ post, hero }) {
   const { category, slug, title } = post
