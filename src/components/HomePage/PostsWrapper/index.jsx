7c0eeb4da82d96d175a02040f61f491504f8a2d5
andre-menezes <andre.menezes@vnda.com.br>
2021-08-31 23:02:04 -0300

    Remover hero da partir da segunda página do blog

--- src/components/HomePage/PostsWrapper/index.jsx
+++ src/components/HomePage/PostsWrapper/index.jsx
@@ -1,16 +1,31 @@
 import React from 'react'
+import { useRouter } from 'next/router'
+import styled from 'styled-components'
 import HeroPostCard from '../HeroPostCard'
 import PostCard from '../../Common/PostCard'
 import Newsletter from '../../Common/Newsletter'
+import { getGutter } from '../../../utils/getGutter'
+
+const StyledPostsWrapper = styled.main`
+  padding-top: ${ ({ isHome }) => !isHome ? getGutter(30) : 'initial' };
+`
 
 export default function PostsWrapper({ postsList, heroRef }) {
+  const router = useRouter()
+  const isHome = router.pathname === '/'
+
   const heroPost = postsList[0]
 
-  const firstBlockStart = 1
+  const firstBlockStart = isHome ? 1 : 0
   const firstBlockEnd = postsList.length / 2
   const firstBlockPosts = postsList.slice(firstBlockStart, firstBlockEnd).map(
     (post, index) => (
-      <PostCard key={post.title} post={post} imageRatio='4x3' index={index} />
+      <PostCard
+        key={post.title}
+        post={post}
+        imageRatio='4x3'
+        heroRef={(index === 0 && !isHome) ? heroRef : null}
+      />
     )
   )
 
@@ -18,16 +33,14 @@ export default function PostsWrapper({ postsList, heroRef }) {
   const secondBlockEnd = postsList.length
   const secondBlockPosts = postsList.slice(
     secondBlockStart, secondBlockEnd
-  ).map((post, index) => (
-    <PostCard key={post.title} post={post} imageRatio='4x3' index={index} />
-  ))
+  ).map(post => <PostCard key={post.title} post={post} imageRatio='4x3' />)
 
   return (
-    <main>
-      <HeroPostCard post={heroPost} heroRef={heroRef} />
+    <StyledPostsWrapper isHome={isHome}>
+      { isHome && <HeroPostCard post={heroPost} heroRef={heroRef} /> }
       {firstBlockPosts}
       <Newsletter />
       {secondBlockPosts}
-    </main>
+    </StyledPostsWrapper>
   )
 }
