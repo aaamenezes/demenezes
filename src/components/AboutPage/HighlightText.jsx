65b743b928e5fb7a65608b6f06a1f63f049cd6f3
André Menezes <andre.menezes@vnda.com.br>
2021-11-11 23:30:00 -0300

    Página Sobre, seção texto em destaque

---
+++ src/components/AboutPage/HighlightText.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const StyledHighlightText = styled.section`
+  padding: 20% 10%;
+  background-image: ${ ({ theme }) => (
+    `linear-gradient(
+      179deg,
+      ${ theme.color.white } 29px,
+      ${ theme.color.blackAlt } 30px,
+      ${ theme.color.blackAlt } calc(100% - 30px),
+      ${ theme.color.white } calc(100% - 29px)
+    )`
+  ) };
+`
+
+const HighlightTitle = styled.h2`
+  color: ${ ({ theme }) => theme.color.gray };
+`
+
+export default function HighlightText() {
+  return (
+    <StyledHighlightText>
+      <HighlightTitle>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque expedita, eveniet repellat eaque totam iste voluptatum voluptatem mollitia fugiat eius tempora error harum fugit? Quas aliquam quod vel ipsam rem.
+      </HighlightTitle>
+    </StyledHighlightText>
+  )
+}
