0b1e9fa622a25d5decfc49e9fd2ce17a67423d71
André Menezes <andre.menezes@vnda.com.br>
2021-11-11 23:29:44 -0300

    Página Sobre, seção categorias

--- pages/about.jsx
+++ pages/about.jsx
@@ -3,17 +3,16 @@
 
 import React from 'react'
 import Greeting from '../src/components/AboutPage/Greeting'
-import Numbers from '../src/components/AboutPage/Numbers'
+import Categories from '../src/components/AboutPage/Categories'
 import pageWrapper from '../src/components/Wrappers/pageWrapper'
+import HighlightText from '../src/components/AboutPage/HighlightText'
 
 function About() {
   return (
     <>
       <Greeting />
-      <Numbers />
-      <h2>Vídeo</h2>
-      <iframe src='http://www.youtube.com/results?search_type=&search_query=video+aula+gimp&gl=BR&hl=pt' frameBorder='0' />
-      <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque expedita, eveniet repellat eaque totam iste voluptatum voluptatem mollitia fugiat eius tempora error harum fugit? Quas aliquam quod vel ipsam rem.</h2>
+      <Categories />
+      <HighlightText />
       <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut totam deserunt harum dignissimos necessitatibus a voluptatibus alias fuga eligendi nihil? Dolor quasi nemo explicabo quo reprehenderit quam, nisi facere delectus?</p>
       <p>Quis quidem quaerat sunt eos animi eveniet impedit nihil et obcaecati beatae commodi voluptas rerum cumque facere numquam, tempora perspiciatis eius deleniti dolores culpa optio veritatis ex incidunt aut! Debitis!</p>
       <p>Nostrum, inventore sint? Beatae eum modi expedita eius, explicabo distinctio voluptates, quod vel animi perspiciatis aperiam assumenda ipsa, ad tempora nostrum? Velit doloremque blanditiis assumenda sunt ullam doloribus, amet eos.</p>
