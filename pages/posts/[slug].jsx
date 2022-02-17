32ff88e09a0a2b979b7a528f7e14be984f4191ff
André Menezes <andre.menezes@vnda.com.br>
2022-02-16 22:34:38 -0300

    Instalar estilizador de código

--- pages/posts/[slug].jsx
+++ pages/posts/[slug].jsx
@@ -75,7 +75,6 @@ export async function getStaticProps({ params }) {
     props: {
       post: {
         ...post,
-        content,
         relatedPosts
       },
       mdxContent
--- src/components/PostPage/PostContent.jsx
+++ src/components/PostPage/PostContent.jsx
@@ -1,5 +1,6 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import styled from 'styled-components'
+import Prism from 'prismjs'
 import { MDXRemote } from 'next-mdx-remote'
 import Container from '../Common/Container'
 
@@ -19,16 +20,15 @@ const StyledPostContent = styled.div`
     margin-bottom: ${ ({ theme }) => `${ theme.spacing.h4 }rem` };
   }
 
-  // Aqui tbm vai a classe do componente de código do Github Gist e vídeos
   p:not(:first-child):not(:last-child),
-  ul:not(:first-child):not(:last-child) {
+  ul:not(:first-child):not(:last-child),
+  pre:not(:first-child):not(:last-child) {
     margin-top: ${ ({ theme }) => `${ theme.spacing.text }rem` };
     margin-bottom: ${ ({ theme }) => `${ theme.spacing.text }rem` };
   }
 
   img:not(:first-child):not(:last-child),
-  iframe:not(:first-child):not(:last-child),
-  .gist .gist-file {
+  iframe:not(:first-child):not(:last-child) {
     margin-top: ${ ({ theme }) => `${ theme.spacing.h1 }rem` };
     margin-bottom: ${ ({ theme }) => `${ theme.spacing.h1 }rem` };
   }
@@ -42,7 +42,13 @@ const StyledPostContent = styled.div`
   }
 `
 
-export default function PostContent({ content, mdxContent, components }) {
+// export default function PostContent({ content, mdxContent, components }) {
+// remover esse no futuro
+export default function PostContent({ mdxContent, components }) {
+  useEffect(() => {
+    Prism.highlightAll()
+  }, [ mdxContent ])
+
   return (
     <Container
       as={StyledPostContent}
