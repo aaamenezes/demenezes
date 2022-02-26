4f8cc445a9a6335a97f51189f52e199e7f29970c
André Menezes <andre.menezes@vnda.com.br>
2022-02-25 22:16:26 -0300

    Corrigir logo do blog

--- src/components/Common/LogoWrapper/index.jsx
+++ src/components/Common/LogoWrapper/index.jsx
@@ -4,19 +4,23 @@ import LinkButton from '../LinkButton'
 
 const LogoLink = styled.a`
   display: block;
+  transition: ${ ({ theme }) => theme.transition.fast };
+
+  &:hover {
+    transform: scale(1.5);
+  }
 `
 
-const Logo = styled.img`
-  margin: 0;
+const Logo = styled.strong`
+  display: block;
+  line-height: ${ ({ theme }) => theme.lineHeight.button };
+  font-size: ${ ({ theme }) => theme.fontSize.h4 };
 `
 
 export default function LogoWrapper() {
   return (
     <LinkButton href='/' as={LogoLink}>
-      <Logo
-        src='https://via.placeholder.com/200x50'
-        alt='Logo do site'
-      />
+      <Logo>deMenezes</Logo>
     </LinkButton>
   )
 }
