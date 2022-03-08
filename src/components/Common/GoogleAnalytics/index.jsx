9b21b630cb925ba51a5be1e753d9f8a833204fa5
André Menezes <andre.menezes@vnda.com.br>
2022-03-07 21:47:59 -0300

    Informações sobre environment no NextJS

--- src/components/Common/GoogleAnalytics/index.jsx
+++ src/components/Common/GoogleAnalytics/index.jsx
@@ -25,3 +25,43 @@ export default function GoogleAnalytics() {
     </>
   )
 }
+
+/**
+ * # .env.production
+ * <!-- DO NOT ADD SECRETS TO THIS FILE. This is a good place for defaults. -->
+ * <!-- If you want to add secrets use `.env.production.local` instead. -->
+ *
+ * ## Variável de produção apenas no servidor
+ * PRODUCTION_ENV_VARIABLE="server_only_production_variable"
+ *
+ * ## Variável de produção pública (AQUI)
+ * NEXT_PUBLIC_PRODUCTION_ENV_VARIABLE="public_production_variable"
+ *
+ * # .env.local.example
+ *
+ * ## Somente variável do ambiente local apenas no servidor
+ * ENV_LOCAL_VARIABLE="server_only_variable_from_env_local"
+ * ## Somente variável do ambiente local pública
+ * NEXT_PUBLIC_ENV_LOCAL_VARIABLE="public_variable_from_env_local"
+ *
+ * # .env.development
+ *
+ * <!-- DO NOT ADD SECRETS TO THIS FILE. This is a good place for defaults. -->
+ * <!-- If you want to add secrets use `.env.development.local` instead. -->
+ *
+ * ## Variável de desenvolvimento apenas no servidor
+ * DEVELOPMENT_ENV_VARIABLE="server_only_development_variable"
+ * ## Variável de desenvolvimento pública
+ * NEXT_PUBLIC_DEVELOPMENT_ENV_VARIABLE="public_development_variable"
+ *
+ * # .env
+ *
+ * <!-- DO NOT ADD SECRETS TO THIS FILE. This is a good place for defaults. -->
+ * <!-- If you want to add secrets use `.env.local` instead. -->
+ *
+ * ## Variável apenas no servidor
+ * ENV_VARIABLE="server_only_variable"
+ *
+ * ## Variável pública
+ * NEXT_PUBLIC_ENV_VARIABLE="public_variable"
+ */
