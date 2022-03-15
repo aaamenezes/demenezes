441d82695eef1d7b582a3081f393af263076f0c6
André Menezes <andre.menezes@vnda.com.br>
2022-03-14 23:46:15 -0300

    Criar componente CodeBlock

--- src/components/PostPage/CodeBlock.jsx
+++ src/components/PostPage/CodeBlock.jsx
@@ -1,22 +1,10 @@
 import React from 'react'
 
 export default function CodeBlock({ children, language }) {
-  const code = children
-  // .split(/ {/g) // troca ' {'
-  // .join('{" {"}') // por '{" {"}'
-  // .split(/ }/g) // troca ' }'
-  // .join('{" }"}') // por '{" }"}'
-  // .split(/^\s{2}/g) // troca '  '
-  // .join('{" "}{" "}') // por '{" "}{" "}' em começo linha
-  // .split(/"}\s{2}/g) // o mesmo que de cima
-  // .join('"}{" "}') // mas após chaves
-  // .split(/\n/g) // corrige as
-  // .join("{'\n'}") // quebras de linha
-
   return (
     <pre className={`language-${ language }`}>
       <code className={`language-${ language }`}>
-        {code}
+        {children}
       </code>
     </pre>
   )
