2b83afa3b22be9caac96b9964ee927c8d039bd00
André Menezes <andre.menezes@vnda.com.br>
2022-03-20 21:12:47 -0300

    Implementar app de comentários Disqus

--- src/components/PostPage/PostComments.jsx
+++ src/components/PostPage/PostComments.jsx
@@ -1,6 +1,28 @@
-import React from 'react'
+/* eslint-disable react/no-this-in-sfc */
+/* eslint-disable no-unused-vars */
+/* eslint-disable camelcase */
+import React, { useEffect } from 'react'
 import Container from '../Common/Container'
 
 export default function PostComments() {
-  return <Container width='lg'>Seção destinada ao Disqus</Container>
+  useEffect(() => {
+    function disqus_config() {
+      this.page.url = window.location.href
+      this.page.identifier = window.location.pathname
+    }
+
+    const script = document.createElement('script')
+    script.src = 'https://demenezes.disqus.com/embed.js'
+    script.setAttribute('data-timestamp', +new Date());
+    (document.head || document.body).appendChild(script)
+  }, [])
+
+  return (
+    <Container width='lg'>
+      <div id='disqus_thread' />
+      <noscript>
+        Please enable JavaScript to view the comments powered by Disqus.
+      </noscript>
+    </Container>
+  )
 }
