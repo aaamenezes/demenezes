4fcf3c5be0b8988155c28a477cb64cb296ce79f5
André Menezes <1992menezes@gmail.com>
2023-04-27 23:34:45 -0300

    Change sitemap.txt for sitemap.xml

--- pages/index.jsx
+++ pages/index.jsx
@@ -3,6 +3,7 @@ import HomeScreen from '../src/components/Screens/HomeScreen'
 import pageWrapper from '../src/components/pageWrapper'
 import { getContent } from '../src/external/datoCMS'
 import { getPaginationInfos } from '../src/utils/getPaginationInfos'
+import { generateSitemap } from '../src/scripts/generate-sitemap.mjs'
 
 function Home({
   CURRENT_POSTS,
@@ -31,6 +32,15 @@ export async function getStaticProps(context) {
     CURRENT_PAGINATION,
     IS_LAST_PAGINATION
   } = getPaginationInfos(allPosts.data.allPosts, params)
+
+  const postsSitemapData = allPosts.data.allPosts.map(
+    post => ({
+      slug: `posts/${ post.slug }`,
+      _updatedAt: post._updatedAt.split('T')[0]
+    })
+  )
+
+  await generateSitemap(postsSitemapData)
 
   return {
     props: {
