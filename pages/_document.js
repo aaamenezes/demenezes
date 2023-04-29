9b0b5e3f24005c91f2304898b45fcf4c95cb1d66
André Menezes <1992menezes@gmail.com>
2023-04-29 00:41:18 -0300

    Add lang="pt-BR"

--- pages/_document.js
+++ pages/_document.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import Document from 'next/document'
+import Document, { Html, Head, Main, NextScript } from 'next/document'
 import { ServerStyleSheet } from 'styled-components'
 
 export default class MyDocument extends Document {
@@ -25,5 +25,17 @@ export default class MyDocument extends Document {
     } finally {
       sheet.seal()
     }
+  }
+
+  render() {
+    return (
+      <Html lang='pt-BR'>
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
   }
 }
