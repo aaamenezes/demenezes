5615352e82b7f5d2e647a6607f5c858403b9b7ad
André Menezes <1992menezes@gmail.com>
2023-05-01 18:05:26 -0300

    Criar página de referências do blog, e puxar dados da primeira referência

--- pages/references.jsx
+++ pages/references.jsx
@@ -9,53 +9,8 @@ const ReferencesContainer = styled.header`
   padding-top: 6.6rem;
 `
 
-const CORS_PROXY = 'https://cors-anywhere.herokuapp.com/'
-
-function getMetadata(html) {
-  const $ = cheerio.load(html)
-
-  const allMetaData = $('head meta').map((index, item) => {
-    if (!item.attribs) return null
-
-    const { property, content } = item.attribs
-    if (!property || !content) return null
-
-    return { property, content }
-  })
-
-  const metaDataMap = {
-    'og:title': 'title',
-    'og:url': 'url',
-    'og:description': 'description',
-    'og:site_name': 'siteName',
-    'og:image': 'image'
-  }
-
-  const metaData = [ ...allMetaData ].reduce((previewData, item) => {
-    const key = metaDataMap[item.property]
-    if (key) previewData[key] = item.content    
-    return previewData
-  }, {})
-
-  return Promise.resolve(metaData)
-}
-
-async function getData(url) {
-  const data = await fetch(CORS_PROXY + url)
-    .then(response => response.text())
-    .then(getMetadata)
-    .catch(console.log)
-
-  return data
-}
-
-function References() {
-  const referencesURLs = [
-    'https://www.alura.com.br/artigos/front-end',
-    'https://www.joshwcomeau.com/'
-  ]
-
-  const referenceData = getData(referencesURLs[0])
+function References({ referencesData }) {
+  console.log('referencesData:', referencesData)
 
   return (
     <Container as={ReferencesContainer} width='lg'>
@@ -65,11 +20,11 @@ function References() {
       </BlockQuote>
       <p>Essa página contém links para todas as referências...</p>
       <ul>
-        {/* {referencesData.map(reference => (
-          <li key={reference}>
-            {reference}
+        {Object.entries(referencesData).map(([ property, content ]) => (
+          <li key={content}>
+            {property}: {content}
           </li>
-        ))} */}
+        ))}
       </ul>
     </Container>
   )
@@ -77,15 +32,57 @@ function References() {
 
 export default pageWrapper(References)
 
-// export async function getStaticProps() {
-// const referencesURLs = await getContent('referencesPage', {})
+export async function getStaticProps() {
+  const CORS_PROXY = 'https://cors-anywhere.herokuapp.com/'
 
-// return {
-//   props: { referencesData: 'a' },
-//   revalidate: 3600
-//   /**
-//    * In secods:
-//    * 60sec * 60 = 1hour = 3600sec
-//    */
-// }
-// }
+  const referencesURLs = [
+    'https://www.alura.com.br/artigos/front-end',
+    'https://www.joshwcomeau.com/'
+  ]
+
+  const referencesData = await fetch(CORS_PROXY + referencesURLs[0], {
+    headers: { Origin: 'https://demenezes.dev/' }
+  })
+    .then(response => response.text())
+    .then(getMetadata)
+    .catch(console.error)
+
+  function getMetadata(html) {
+    const $ = cheerio.load(html)
+
+    const allMetaData = $('head meta').map((index, item) => {
+      if (!item.attribs) return null
+
+      const { property, content } = item.attribs
+      if (!property || !content) return null
+
+      return { property, content }
+    })
+
+    const metaDataMap = {
+      'og:title': 'title',
+      'og:url': 'url',
+      'og:description': 'description',
+      'og:site_name': 'siteName',
+      'og:image': 'image'
+    }
+
+    const metaData = [ ...allMetaData ].reduce((previewData, item) => {
+      const key = metaDataMap[item.property]
+      if (key) previewData[key] = item.content    
+      return previewData
+    }, {})
+
+    return Promise.resolve(metaData)
+  }
+
+
+  return {
+    props: { referencesData },
+    revalidate: 3600
+    /**
+     * In secods:
+     * 60sec * 60 = 1hour = 3600sec
+     */
+  }
+}
