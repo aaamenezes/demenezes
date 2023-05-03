f39d1968ce96e1a0b35116efdf573d7d4ef041da
André Menezes <1992menezes@gmail.com>
2023-05-02 22:28:41 -0300

    Remover lógica das referências e trocar pela lib url-meta-scraper. Apresentar primeiros resultados na tela vindo do DatoCMS

--- pages/references.jsx
+++ pages/references.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import styled from 'styled-components'
+import { parser } from 'url-meta-scraper'
 import pageWrapper from '../src/components/pageWrapper'
 import Container from '../src/components/Common/Container'
 import BlockQuote from '../src/components/Common/BlockQuote'
 import { getContent } from '../src/external/datoCMS'
-import { getMetadataByURL } from '../src/external/getMetadataByURL'
 
 const ReferencesContainer = styled.header`
   padding-top: 6.6rem;
@@ -19,12 +19,22 @@ function References({ referencesData }) {
       </BlockQuote>
       <p>Essa página contém links para todas as referências...</p>
       <ul>
-        {Object.entries(referencesData).map(([ property, content ]) => (
-          <li key={content}>
-            <strong>{property}</strong>
-            :
-            {' '}
-            { content }
+        {referencesData.map(reference => (
+          <li key={reference.url}>
+            <h2>{reference.title}</h2>
+            <p>{reference.description}</p>
+            <p>
+              <a href={reference.url}>Acessar</a>
+            </p>
+            <p>
+              <small>{reference.referenceType}</small>
+            </p>
+            <p>
+              <img
+                alt={`Imagem de capa do blog ${ reference.title }`}
+                src={reference.image}
+              />
+            </p>
           </li>
         ))}
       </ul>
@@ -35,22 +45,38 @@ function References({ referencesData }) {
 export default pageWrapper(References)
 
 export async function getStaticProps() {
-  // const referencesDatabase = await getContent('allReferences', {})
-  // const { allReferences } = referencesDatabase.data
+  const referencesDatabase = await getContent('allReferences', {})
+  const { allReferences } = referencesDatabase.data
 
-  const allReferences = [
-    { url: 'https://www.alura.com.br/artigos/front-end', type: 'blog' },
-    { url: 'https://www.joshwcomeau.com/', type: 'blog' }
-  ]
+  const promises = allReferences.map(
+    async reference => [ reference, await parser(reference.url) ]
+  )
 
-  const referencesData = await getMetadataByURL(allReferences)
+  function getData(result) {
+    const [ firstPart, secondPart ] = result.value
+    const { og } = secondPart
+    if (!og) return false
+
+    const { url, referenceType } = firstPart
+
+    return {
+      title: og.title || null,
+      url,
+      description: og.description || null,
+      image: og.image || null,
+      referenceType
+    }
+  }
+
+  const referencesData = await Promise.allSettled(promises)
+    .then(results => results.map(getData))
 
   return {
     props: { referencesData },
-    revalidate: 3600
+    revalidate: 86400
     /**
      * In secods:
-     * 60sec * 60 = 1hour = 3600sec
+     * 60sec * 60 = 1hour * 24 = 86400sec = 1 day
      */
   }
 }
