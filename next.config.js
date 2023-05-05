7abf9bd8fb5ac781c911572cb02f1610d98436f5
André Menezes <1992menezes@gmail.com>
2023-05-04 22:57:05 -0300

    Add domain do trustDomain to images in References page

--- next.config.js
+++ next.config.js
@@ -1,13 +1,26 @@
+const trustDomains = [
+  'www.datocms-assets.com',
+  'mariosouto.com',
+  'substackcdn.com',
+  'www.tabnews.com.br',
+  'cdn.shopify.com',
+  'blog.rdstation.com',
+  'raw.githubusercontent.com',
+  'www.joshwcomeau.com',
+  'flaviocopes.com',
+  'i0.wp.com',
+  'www.agenciamestre.com',
+  'www.alura.com.br'
+]
+
 module.exports = {
   trailingSlash: true,
   experimental: { optimizeCss: true },
   images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'www.datocms-assets.com',
-        port: ''
-      }
-    ]
+    remotePatterns: trustDomains.map(domain => ({
+      protocol: 'https',
+      hostname: domain,
+      port: ''
+    }))
   }
 }
