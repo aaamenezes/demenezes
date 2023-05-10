508f8e8dfde9c1d3bcdc5bc645aba96bcee31335
André Menezes <1992menezes@gmail.com>
2023-05-10 20:55:20 -0300

    Componentizar PaginationButton

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -3,6 +3,7 @@ import styled, { css } from 'styled-components'
 import getBreakpoints from '../utils/getBreakpoints'
 import Container from './Common/Container'
 import LinkButton from './Common/LinkButton'
+import { PaginationButton } from './PaginationButton'
 
 const PaginationWrapper = styled.div`
   ${ getBreakpoints({
@@ -10,57 +11,16 @@ const PaginationWrapper = styled.div`
       display: flex;
       flex-direction: column;
       align-items: center;
-    `,
-    md: css`
-      flex-direction: row;
-      justify-content: flex-end;
-    `
-  }) }
-`
 
-const PaginationButton = styled.a`
-  ${ getBreakpoints({
-    xs: css`
-      position: relative;
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      width: 100%;
-      text-align: center;
-      line-height: ${ ({ theme }) => theme.lineHeight.button };
-      padding: 0.66rem;
-      border-radius: ${ ({ theme }) => theme.borderRadius };
-      border: 1px solid ${ ({ theme }) => theme.color.neutral_900 };
-      transition: ${ ({ theme }) => theme.transition.fast };
-
-      &::after {
-        content: '';
-        position: absolute;
-        z-index: ${ ({ theme }) => theme.zIndex.under };
-        ${ ({ label }) => label === 'previous' ? 'right: 0;' : 'left: 0;' }
-        height: 100%;
-        width: 0;
-        background-color: ${ ({ theme }) => theme.color.red_500 };
-        transition: ${ ({ theme }) => theme.transition.fast };
-      }
-
-      &:hover,
-      &:focus {
-        color: ${ ({ theme }) => theme.color.neutral_50 };
-        border-color: ${ ({ theme }) => theme.color.red_500 };
-        &::after {
-          width: 100%;
-        }
-      }
-
-      &:nth-child(2) {
+      > a:nth-child(2) {
         margin: 2.2rem 0 0;
       }
     `,
     md: css`
-      width: initial;
+      flex-direction: row;
+      justify-content: flex-end;
 
-      &:nth-child(2) {
+      > a:nth-child(2) {
         margin: 0;
         margin-left: auto;
       }
---
+++ src/components/PaginationButton.jsx
@@ -0,0 +1,45 @@
+import styled, { css } from 'styled-components'
+import getBreakpoints from '../utils/getBreakpoints'
+
+export const PaginationButton = styled.a`
+  ${ getBreakpoints({
+    xs: css`
+      position: relative;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      width: 100%;
+      text-align: center;
+      text-transform: uppercase;
+      line-height: ${ ({ theme }) => theme.lineHeight.button };
+      padding: 0.66rem;
+      border-radius: ${ ({ theme }) => theme.borderRadius };
+      border: 1px solid ${ ({ theme }) => theme.color.neutral_900 };
+      transition: ${ ({ theme }) => theme.transition.fast };
+      cursor: pointer;
+
+      &::after {
+        content: '';
+        position: absolute;
+        z-index: ${ ({ theme }) => theme.zIndex.under };
+        ${ ({ label }) => label === 'previous' ? 'right: 0;' : 'left: 0;' }
+        height: 100%;
+        width: 0;
+        background-color: ${ ({ theme }) => theme.color.red_500 };
+        transition: ${ ({ theme }) => theme.transition.fast };
+      }
+
+      &:hover,
+      &:focus {
+        color: ${ ({ theme }) => theme.color.neutral_50 };
+        border-color: ${ ({ theme }) => theme.color.red_500 };
+        &::after {
+          width: 100%;
+        }
+      }
+    `,
+    md: css`
+      width: initial;
+    `
+  }) }
+`
