67d8d19339682b9ae3b6437270a09c69d0704be6
André Menezes <1992menezes@gmail.com>
2024-12-18 23:03:27 -0300

    type hobbies component

--- src/components/AboutPage/Hobbies.tsx
+++ src/components/AboutPage/Hobbies.tsx
@@ -6,7 +6,13 @@ const HobbiesTitle = styled.h2`
   margin-top: ${ ({ theme }) => `${ theme.spacing.h2 }rem` };
 `
 
-export default function Hobbies({ hobbiesTitle, hobbiesText }) {
+export default function Hobbies({
+  hobbiesTitle,
+  hobbiesText
+}: {
+  hobbiesTitle: string,
+  hobbiesText: string
+}) {
   return (
     <Container as='section' width='md'>
       <HobbiesTitle>{hobbiesTitle}</HobbiesTitle>
