09d9d47499ea68e5ca5d8a9d73d5948d6677b785
André Menezes <1992menezes@gmail.com>
2024-12-19 22:35:06 -0300

    type ga

--- src/components/Common/GoogleAnalytics/gtag.ts
+++ src/components/Common/GoogleAnalytics/gtag.ts
@@ -1,14 +1,30 @@
+declare global {
+  interface Window {
+    gtag: (...args: any[]) => void;
+  }
+}
+
 export const GA_TRACKING_ID = process.env.NEXT_PUBLIC_GA_ID
 
 // https://developers.google.com/analytics/devguides/collection/gtagjs/pages
-export function pageview(url) {
+export function pageview(url: string) {
   window.gtag('config', GA_TRACKING_ID, {
     page_path: url
   })
 }
 
 // https://developers.google.com/analytics/devguides/collection/gtagjs/events
-export function event({ action, category, label, value }) {
+export function event({
+  action,
+  category,
+  label,
+  value
+}: {
+  action: string
+  category: string
+  label: string
+  value: number
+}) {
   window.gtag('event', action, {
     event_category: category,
     event_label: label,
