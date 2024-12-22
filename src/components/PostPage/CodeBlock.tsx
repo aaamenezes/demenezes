bb34fa9b283ca298ec6d430322f16c4e142684fe
André Menezes <1992menezes@gmail.com>
2024-12-21 23:32:21 -0300

    type codeblock

--- src/components/PostPage/CodeBlock.tsx
+++ src/components/PostPage/CodeBlock.tsx
@@ -1,6 +1,6 @@
-import React from 'react'
+import React, { PropsWithChildren } from 'react'
 
-export default function CodeBlock({ children, language }) {
+export default function CodeBlock({ children, language }: PropsWithChildren<{ language: string }>) {
   return (
     <pre className={`language-${ language }`}>
       <code className={`language-${ language }`}>
