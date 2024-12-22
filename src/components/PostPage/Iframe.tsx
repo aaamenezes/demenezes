89fad3eafa6404a9db83346874b683fbc7d7e10a
André Menezes <1992menezes@gmail.com>
2024-12-21 23:33:10 -0300

    type iframe

--- src/components/PostPage/Iframe.tsx
+++ src/components/PostPage/Iframe.tsx
@@ -5,13 +5,13 @@ const StyledIframe = styled.iframe`
   width: 100%;
 `
 
-export default function Iframe({ title, videoID }) {
+export default function Iframe({ title, videoID }: { title: string, videoID: string }) {
   return (
     <StyledIframe
       title={title}
       height='480'
       src={`https://www.youtube.com/embed/${ videoID }`}
-      frameBorder='0'
+      frame-border='0'
       allow='accelerometer;clipboard-write;encrypted-media;picture-in-picture'
       allowFullScreen
     />
