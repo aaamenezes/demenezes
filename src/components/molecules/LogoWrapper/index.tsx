1172955b5639523163e988ce874288217ade4b26
André Menezes <1992menezes@gmail.com>
2025-02-08 15:54:20 -0300

    swap style on logo wrapper component

--- src/components/molecules/LogoWrapper/index.tsx
+++ src/components/molecules/LogoWrapper/index.tsx
@@ -1,42 +1,12 @@
-import React from 'react';
-import styled from 'styled-components';
 import LinkButton from '../../atoms/LinkButton';
-
-const LogoLink = styled(LinkButton)`
-  position: relative;
-  display: block;
-  transition: ${({ theme }) => theme.transition.fast};
-
-  &::after {
-    content: '';
-    position: absolute;
-    transform: translateX(-50%);
-    left: 50%;
-    bottom: 0;
-    width: 0;
-    height: 2px;
-    background-color: ${({ theme }) => theme.color.neutral_100};
-    transition: ${({ theme }) => theme.transition.fast};
-  }
-
-  &:hover::after,
-  &:focus::after {
-    width: 100%;
-  }
-`;
-
-const Logo = styled.strong`
-  display: block;
-  line-height: ${({ theme }) => theme.lineHeight.button};
-  font-size: ${({ theme }) => theme.fontSize.h4};
-  font-family: ${({ theme }) => theme.fontFamily.text};
-`;
+import S from './styles.module.css';
 
 export default function LogoWrapper() {
   const preview = false;
+  // mudar strong para h1
   return (
-    <LogoLink href="/">
-      <Logo>{preview ? 'Preview' : 'deMenezes'}</Logo>
-    </LogoLink>
+    <LinkButton className={S.logoLink} href="/">
+      <strong className={S.logo}>{preview ? 'Preview' : 'deMenezes'}</strong>
+    </LinkButton>
   );
 }
