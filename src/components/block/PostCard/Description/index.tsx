e2902b02332177e4ff6dc0eaf02c98c9e7d3c2c1
André Menezes <1992menezes@gmail.com>
2025-02-09 15:13:41 -0300

    swap style in post card description component

--- src/components/block/PostCard/Description/index.tsx
+++ src/components/block/PostCard/Description/index.tsx
@@ -1,31 +1,16 @@
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-nocheck
-import React from 'react';
-import styled, { css } from 'styled-components';
-import getBreakpoints from '../../../utils/getBreakpoints';
-
-const PostCardDescriptionWrapper = styled.p`
-  ${getBreakpoints({
-    xs: css`
-      display: ${({ isCompact }) => (isCompact ? 'none' : 'block')};
-      padding: 0.5rem;
-      margin: 1.1rem 0;
-    `,
-  })}
-`;
+import { clsx } from '../../../../utils/clsx';
+import S from './styles.module.css';
 
 export default function PostCardDescription({
   description,
-  hero,
   isCompact,
 }: {
   description: string;
-  hero?: boolean;
   isCompact: boolean;
 }) {
   return (
-    <PostCardDescriptionWrapper hero={hero} isCompact={isCompact}>
+    <p className={clsx(S.postCardDescriptionWrapper, isCompact && 'isCompact')}>
       {description}
-    </PostCardDescriptionWrapper>
+    </p>
   );
 }
