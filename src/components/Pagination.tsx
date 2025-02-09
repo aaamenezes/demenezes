0039cb765cb9c899f21a01bda89c245136bad14b
André Menezes <1992menezes@gmail.com>
2025-02-09 20:01:38 -0300

    adjust pagination component

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -1,7 +1,9 @@
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-nocheck
 import styled, { css } from 'styled-components';
 import getBreakpoints from '../utils/getBreakpoints';
 import Container from './Common/Container';
-import { PaginationButton } from './PaginationButton';
+import LinkButton from './base/LinkButton';
 
 const PaginationWrapper = styled.div`
   ${getBreakpoints({
@@ -26,6 +28,54 @@ const PaginationWrapper = styled.div`
   })}
 `;
 
+const PaginationButton = styled(LinkButton)<{
+  label: string;
+  disabled?: boolean;
+}>`
+  ${getBreakpoints({
+    xs: css`
+      position: relative;
+      display: inline-flex;
+      justify-content: center;
+      align-items: center;
+      width: 100%;
+      text-align: center;
+      text-transform: uppercase;
+      line-height: ${({ theme }) => theme.lineHeight.button};
+      padding: 0.66rem;
+      border-radius: ${({ theme }) => theme.borderRadius};
+      border: 1px solid
+        ${({ theme, disabled }) =>
+          theme.color.neutral_900 + (disabled ? theme.opacity.light.hex : '')};
+      transition: ${({ theme }) => theme.transition.fast};
+      cursor: pointer;
+
+      &::after {
+        content: '';
+        position: absolute;
+        z-index: ${({ theme }) => theme.zIndex.under};
+        ${({ label }) => (label === 'previous' ? 'right: 0;' : 'left: 0;')}
+        height: 100%;
+        width: 0;
+        background-color: ${({ theme }) => theme.color.red_500};
+        transition: ${({ theme }) => theme.transition.fast};
+      }
+
+      &:hover,
+      &:focus {
+        color: ${({ theme }) => theme.color.neutral_50};
+        border-color: ${({ theme }) => theme.color.red_500};
+        &::after {
+          width: 100%;
+        }
+      }
+    `,
+    md: css`
+      width: initial;
+    `,
+  })}
+`;
+
 export default function Pagination({
   currentPagination,
   isLastPagination,
@@ -39,14 +89,16 @@ export default function Pagination({
   return (
     <Container as={PaginationWrapper} width="xxl" spacing={2}>
       {currentPagination > 1 && (
-        <PaginationButton href={previousLink}>
+        <PaginationButton href={previousLink} label="previous">
           Posts mais recentes
         </PaginationButton>
       )}
       {!isLastPagination ? (
-        <PaginationButton href={nextLink}>Próxima página</PaginationButton>
+        <PaginationButton href={nextLink} label="next">
+          Próxima página
+        </PaginationButton>
       ) : (
-        <PaginationButton href="/">
+        <PaginationButton href="/" label="next" disabled>
           Parabéns, você chegou ao início do blog :)
         </PaginationButton>
       )}
