693b1d977cea5e62f1bbfca26a4df84a124d324f
André Menezes <1992menezes@gmail.com>
2025-02-16 14:57:05 -0300

    swap styles in about categories component

--- src/components/section/AboutCategories/index.tsx
+++ src/components/section/AboutCategories/index.tsx
@@ -1,85 +1,7 @@
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-nocheck
-import styled, { css } from 'styled-components';
-import getBreakpoints from '../../../utils/getBreakpoints';
 import Icon from '../../base/Icon';
+import { IconName } from '../../base/Icon/types';
 import Wrapper from '../../container/Wrapper';
-
-const CategoriesTitle = styled.h2`
-  margin-top: ${({ theme }) => `${theme.spacing.h2}rem`};
-`;
-
-const CategoriesList = styled.ul`
-  ${getBreakpoints({
-    xs: css`
-      display: grid;
-      grid-template-columns: 1fr;
-      grid-gap: 1rem;
-      margin: 0;
-      text-align: center;
-    `,
-    md: css`
-      grid-template-columns: 1fr 1fr;
-      grid-gap: 2rem;
-    `,
-    xl: css`
-      grid-template-columns: 1fr 1fr 1fr;
-      grid-gap: 3rem;
-    `,
-  })}
-`;
-
-const CategoryItem = styled.li`
-  ${getBreakpoints({
-    xs: css`
-      display: flex;
-      flex-direction: column;
-      justify-content: space-between;
-      width: 100%;
-      padding: 2rem;
-      margin: 0 auto;
-      box-shadow: none;
-      transition: ${({ theme }) => theme.transition.fast};
-
-      &:hover {
-        box-shadow: ${({ theme }) => theme.boxShadow.high};
-
-        > * {
-          opacity: 1;
-        }
-      }
-    `,
-    md: css`
-      max-width: initial;
-      margin: initial;
-    `,
-  })}
-`;
-
-const CategoryItemTitle = styled.h3`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  margin-top: 0;
-  margin-bottom: 1rem;
-`;
-
-const CategoryText = styled.p`
-  ${getBreakpoints({
-    md: css`
-      flex: 1;
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      margin: 0;
-      transition: ${({ theme }) => theme.transition.fast};
-    `,
-    xl: css`
-      opacity: 0;
-    `,
-  })}
-`;
+import S from './styles.module.css';
 
 export default function AboutCategories({
   categoriesTitle,
@@ -94,23 +16,24 @@ export default function AboutCategories({
 }) {
   const CategoryItems = categoryItems.map(
     ({ categoryItemIcon, categoryItemTitle, categoryItemDescription }) => (
-      <CategoryItem key={categoryItemTitle}>
-        <CategoryItemTitle>
-          <Icon name={categoryItemIcon} color="black" />
+      <li className={S.categoryItem} key={categoryItemTitle}>
+        <h3 className={S.categoryItemTitle}>
+          {/* não gostei desse "as IconName", corrigir isso */}
+          <Icon name={categoryItemIcon as IconName} color="black" />
           <span>{categoryItemTitle}</span>
-        </CategoryItemTitle>
-        <CategoryText>{categoryItemDescription}</CategoryText>
-      </CategoryItem>
+        </h3>
+        <p className={S.categoryText}>{categoryItemDescription}</p>
+      </li>
     )
   );
 
   return (
     <Wrapper as="section" width="xl">
       <header>
-        <CategoriesTitle>{categoriesTitle}</CategoriesTitle>
+        <h2 className={S.categoriesTitle}>{categoriesTitle}</h2>
       </header>
 
-      <CategoriesList>{CategoryItems}</CategoriesList>
+      <ul className={S.categoriesList}>{CategoryItems}</ul>
     </Wrapper>
   );
 }
