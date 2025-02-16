b459c1a7950268497e48b608b33536b7fe73aefd
André Menezes <1992menezes@gmail.com>
2025-02-15 23:17:29 -0300

    create new navigation component

--- src/components/block/ToggleModalButton/index.tsx
+++ src/components/block/ToggleModalButton/index.tsx
@@ -2,24 +2,25 @@ import type { CSSProperties } from 'react';
 import Icon from '../../base/Icon';
 import { IconName } from '../../base/Icon/types';
 import S from './styles.module.css';
+import { clsx } from '../../../utils/clsx';
 
 export default function ToggleModalButton({
   onClick = () => {},
   icon,
   color = 'currentColor',
-  ...props
+  className = '',
 }: {
   onClick: () => void;
   icon: IconName;
   color?: string;
+  className?: string;
 }) {
   return (
     <button
-      className={S.button}
+      className={clsx(S.button, className)}
       style={{ '--button-color': color } as CSSProperties}
       onClick={onClick}
       color={color}
-      {...props}
     >
       <Icon className={S.i} name={icon} />
     </button>
