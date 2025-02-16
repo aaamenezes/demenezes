8e0fc98ffe5996320ad185e0e2d6da90d7da3cf0
André Menezes <1992menezes@gmail.com>
2025-02-16 20:13:05 -0300

    create new about greeting component

---
+++ src/components/section/AboutGreeting/index.tsx
@@ -0,0 +1,41 @@
+import NextImage from 'next/image';
+import HiddenA11Y from '../../base/HiddenA11Y';
+import Wrapper from '../../container/Wrapper';
+import S from './styles.module.css';
+
+export default function Greeting({
+  greetingTitle,
+  greetingSubtitle,
+  greetingDescription,
+  profileImage,
+}: {
+  greetingTitle: string;
+  greetingSubtitle: string;
+  greetingDescription: string;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  profileImage: any;
+}) {
+  const { src } = profileImage.responsiveImage;
+  const { alt, width, height } = profileImage;
+
+  return (
+    <Wrapper className={S.greeting} width="lg">
+      <div className={S.imageWrapper}>
+        <NextImage
+          src={src}
+          alt={alt}
+          sizes="(max-width: 767px) 100vw,
+                 (max-width: 1199px) 50vw,
+                                     33vw"
+          width={width}
+          height={height}
+        />
+      </div>
+      <h1>
+        <HiddenA11Y>{greetingTitle}</HiddenA11Y>
+      </h1>
+      <h2 className={S.greetingTitle}>{greetingSubtitle}</h2>
+      <p className={S.greetingText}>{greetingDescription}</p>
+    </Wrapper>
+  );
+}
