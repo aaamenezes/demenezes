bf98024b697ccc9e96e9f8fa03f7c02f641a47bb
André Menezes <1992menezes@gmail.com>
2025-03-03 16:23:47 -0300

    create new RelatedPosts components

---
+++ src/components/ui/section/RelatedPosts/index.tsx
@@ -0,0 +1,27 @@
+import { PostSummaryProps } from '../../../../types';
+import PostCard from '../../block/PostCard';
+import Wrapper from '../../container/Wrapper';
+import S from './styles.module.css';
+
+export default function RelatedPosts({
+  postCategory,
+  relatedPosts,
+}: {
+  postCategory: string;
+  relatedPosts: PostSummaryProps[];
+}) {
+  if (relatedPosts.length === 0) return <></>;
+
+  const relatedPostsElements = relatedPosts.map(post => (
+    <PostCard key={post.title} post={post} isCompact spacing={0} />
+  ));
+
+  return (
+    <Wrapper as="section" width="xl">
+      <header>
+        <h2>{`Veja outros posts sobre ${postCategory}`}</h2>
+      </header>
+      <div className={S.relatedPostsGrid}>{relatedPostsElements}</div>
+    </Wrapper>
+  );
+}
