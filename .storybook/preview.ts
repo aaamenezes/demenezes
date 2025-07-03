8b89d41036579133a908ba906f787db28fb91b18
André Menezes <1992menezes@gmail.com>
2025-07-02 23:17:26 -0300

    import theme in storybook

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,11 +1,13 @@
-import type { Preview } from '@storybook/react'
+import type { Preview } from '@storybook/react';
+import '../styles/variables.css';
+import '../styles/reset.css';
 
 const preview: Preview = {
   parameters: {
     controls: {
       matchers: {
-       color: /(background|color)$/i,
-       date: /Date$/i,
+        color: /(background|color)$/i,
+        date: /Date$/i,
       },
     },
   },
