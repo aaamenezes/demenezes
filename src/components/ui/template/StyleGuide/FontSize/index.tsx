a59387c69cbaf358f83baecd71aa061a24a7ffe5
André Menezes <1992menezes@gmail.com>
2025-09-22 07:57:36 -0300

    feat: add FontSize component and associated styles

---
+++ src/components/ui/template/StyleGuide/FontSize/index.tsx
@@ -0,0 +1,73 @@
+import Wrapper from '../../../container/Wrapper';
+import styles from './styles.module.css';
+
+export default function FontSize() {
+  return (
+    <Wrapper width="full">
+      <h2 className={styles.subHeading}>Font-size</h2>
+      <h3 className={styles.subSubHeading}>fontSizeXxxs</h3>
+      <p className={styles.fontSizeXxxs}>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellendus
+        eum adipisci totam ut, natus at, saepe earum consequatur doloremque,
+        dicta doloribus nam explicabo iste nostrum reprehenderit non modi eius
+        laudantium.
+      </p>
+      <h3 className={styles.subSubHeading}>fontSizeXxs</h3>
+      <p className={styles.fontSizeXxs}>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellendus
+        eum adipisci totam ut, natus at, saepe earum consequatur doloremque,
+        dicta doloribus nam explicabo iste nostrum reprehenderit non modi eius
+        laudantium.
+      </p>
+      <h3 className={styles.subSubHeading}>fontSizeXs (base)</h3>
+      <p className={styles.fontSizeXs}>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellendus
+        eum adipisci totam ut, natus at, saepe earum consequatur doloremque,
+        dicta doloribus nam explicabo iste nostrum reprehenderit non modi eius
+        laudantium.
+      </p>
+      <h3 className={styles.subSubHeading}>fontSizeSm</h3>
+      <p className={styles.fontSizeSm}>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellendus
+        eum adipisci totam ut, natus at, saepe earum consequatur doloremque,
+        dicta doloribus nam explicabo iste nostrum reprehenderit non modi eius
+        laudantium.
+      </p>
+      <h3 className={styles.subSubHeading}>fontSizeMd</h3>
+      <p className={styles.fontSizeMd}>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellendus
+        eum adipisci totam ut, natus at, saepe earum consequatur doloremque,
+        dicta doloribus nam explicabo iste nostrum reprehenderit non modi eius
+        laudantium.
+      </p>
+      <h3 className={styles.subSubHeading}>fontSizeLg</h3>
+      <p className={styles.fontSizeLg}>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellendus
+        eum adipisci totam ut, natus at, saepe earum consequatur doloremque,
+        dicta doloribus nam explicabo iste nostrum reprehenderit non modi eius
+        laudantium.
+      </p>
+      <h3 className={styles.subSubHeading}>fontSizeXl</h3>
+      <p className={styles.fontSizeXl}>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellendus
+        eum adipisci totam ut, natus at, saepe earum consequatur doloremque,
+        dicta doloribus nam explicabo iste nostrum reprehenderit non modi eius
+        laudantium.
+      </p>
+      <h3 className={styles.subSubHeading}>fontSizeXxl</h3>
+      <p className={styles.fontSizeXxl}>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellendus
+        eum adipisci totam ut, natus at, saepe earum consequatur doloremque,
+        dicta doloribus nam explicabo iste nostrum reprehenderit non modi eius
+        laudantium.
+      </p>
+      <h3 className={styles.subSubHeading}>fontSizeXxxl</h3>
+      <p className={styles.fontSizeXxxl}>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellendus
+        eum adipisci totam ut, natus at, saepe earum consequatur doloremque,
+        dicta doloribus nam explicabo iste nostrum reprehenderit non modi eius
+        laudantium.
+      </p>
+    </Wrapper>
+  );
+}
