ec95590334f128ca815aa4809aef6746de93ab07
André Menezes <1992menezes@gmail.com>
2025-09-22 08:10:27 -0300

    refactor: replace heading elements with reusable SubHeading and SubSubHeading components

--- src/components/ui/template/StyleGuide/FontSize/index.tsx
+++ src/components/ui/template/StyleGuide/FontSize/index.tsx
@@ -1,67 +1,69 @@
 import Wrapper from '../../../container/Wrapper';
+import SubHeading from '../base/SubHeading';
+import SubSubHeading from '../base/SubSubHeading';
 import styles from './styles.module.css';
 
 export default function FontSize() {
   return (
     <Wrapper width="full">
-      <h2 className={styles.subHeading}>Font-size</h2>
-      <h3 className={styles.subSubHeading}>fontSizeXxxs</h3>
+      <SubHeading>Font-size</SubHeading>
+      <SubSubHeading>fontSizeXxxs</SubSubHeading>
       <p className={styles.fontSizeXxxs}>
         Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellendus
         eum adipisci totam ut, natus at, saepe earum consequatur doloremque,
         dicta doloribus nam explicabo iste nostrum reprehenderit non modi eius
         laudantium.
       </p>
-      <h3 className={styles.subSubHeading}>fontSizeXxs</h3>
+      <SubSubHeading>fontSizeXxs</SubSubHeading>
       <p className={styles.fontSizeXxs}>
         Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellendus
         eum adipisci totam ut, natus at, saepe earum consequatur doloremque,
         dicta doloribus nam explicabo iste nostrum reprehenderit non modi eius
         laudantium.
       </p>
-      <h3 className={styles.subSubHeading}>fontSizeXs (base)</h3>
+      <SubSubHeading>fontSizeXs (base)</SubSubHeading>
       <p className={styles.fontSizeXs}>
         Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellendus
         eum adipisci totam ut, natus at, saepe earum consequatur doloremque,
         dicta doloribus nam explicabo iste nostrum reprehenderit non modi eius
         laudantium.
       </p>
-      <h3 className={styles.subSubHeading}>fontSizeSm</h3>
+      <SubSubHeading>fontSizeSm</SubSubHeading>
       <p className={styles.fontSizeSm}>
         Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellendus
         eum adipisci totam ut, natus at, saepe earum consequatur doloremque,
         dicta doloribus nam explicabo iste nostrum reprehenderit non modi eius
         laudantium.
       </p>
-      <h3 className={styles.subSubHeading}>fontSizeMd</h3>
+      <SubSubHeading>fontSizeMd</SubSubHeading>
       <p className={styles.fontSizeMd}>
         Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellendus
         eum adipisci totam ut, natus at, saepe earum consequatur doloremque,
         dicta doloribus nam explicabo iste nostrum reprehenderit non modi eius
         laudantium.
       </p>
-      <h3 className={styles.subSubHeading}>fontSizeLg</h3>
+      <SubSubHeading>fontSizeLg</SubSubHeading>
       <p className={styles.fontSizeLg}>
         Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellendus
         eum adipisci totam ut, natus at, saepe earum consequatur doloremque,
         dicta doloribus nam explicabo iste nostrum reprehenderit non modi eius
         laudantium.
       </p>
-      <h3 className={styles.subSubHeading}>fontSizeXl</h3>
+      <SubSubHeading>fontSizeXl</SubSubHeading>
       <p className={styles.fontSizeXl}>
         Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellendus
         eum adipisci totam ut, natus at, saepe earum consequatur doloremque,
         dicta doloribus nam explicabo iste nostrum reprehenderit non modi eius
         laudantium.
       </p>
-      <h3 className={styles.subSubHeading}>fontSizeXxl</h3>
+      <SubSubHeading>fontSizeXxl</SubSubHeading>
       <p className={styles.fontSizeXxl}>
         Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellendus
         eum adipisci totam ut, natus at, saepe earum consequatur doloremque,
         dicta doloribus nam explicabo iste nostrum reprehenderit non modi eius
         laudantium.
       </p>
-      <h3 className={styles.subSubHeading}>fontSizeXxxl</h3>
+      <SubSubHeading>fontSizeXxxl</SubSubHeading>
       <p className={styles.fontSizeXxxl}>
         Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellendus
         eum adipisci totam ut, natus at, saepe earum consequatur doloremque,
