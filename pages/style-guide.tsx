e85655bd990562e45be6c533933434e4ed571bfe
André Menezes <1992menezes@gmail.com>
2025-09-22 07:57:01 -0300

    feat: add StyleGuide page

---
+++ pages/style-guide.tsx
@@ -0,0 +1,11 @@
+import Head from '../src/components/meta/Head';
+import StyleGuideScreen from '../src/components/ui/template/StyleGuide';
+
+export default function StyleGuide() {
+  return (
+    <>
+      <Head componentProps={{}} />
+      <StyleGuideScreen />
+    </>
+  );
+}
