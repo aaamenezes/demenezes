934562c6c0d7a00c9c3cfe529f8da2a09cbf6da2
André Menezes <1992menezes@gmail.com>
2025-10-30 10:50:04 -0300

    Update imports on AboutContactForm component

--- src/components/ui/section/AboutContactForm/index.tsx
+++ src/components/ui/section/AboutContactForm/index.tsx
@@ -1,6 +1,6 @@
+import Wrapper from '@/components/ui/container/Wrapper';
 import settings from '@/data/settings.json';
-import { ContactPageProps } from '../../../../types';
-import Wrapper from '../../container/Wrapper';
+import { ContactPageProps } from '@/types';
 import style from './styles.module.css';
 
 export default function ContactForm({
