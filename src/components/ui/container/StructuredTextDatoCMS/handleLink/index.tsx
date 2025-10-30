868597c506cb3b380237466b365f18df53c29620
André Menezes <1992menezes@gmail.com>
2025-10-30 10:44:57 -0300

    Update imports on HandleLink component

--- src/components/ui/container/StructuredTextDatoCMS/handleLink/index.tsx
+++ src/components/ui/container/StructuredTextDatoCMS/handleLink/index.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import Link from '../../../base/Link';
+import Link from '@/components/ui/base/Link';
 import style from './styles.module.css';
 
 export default function HandleLink({ node }: any) {
