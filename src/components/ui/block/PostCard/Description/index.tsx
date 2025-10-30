19993fb9335e432a11747be6df9965f940472358
André Menezes <1992menezes@gmail.com>
2025-10-30 07:46:28 -0300

    Update imports on PostDescription component

--- src/components/ui/block/PostCard/Description/index.tsx
+++ src/components/ui/block/PostCard/Description/index.tsx
@@ -1,6 +1,6 @@
-import { clsx } from '../../../../../utils/clsx';
+import fontStyle from '@/../styles/font-style.module.css';
+import { clsx } from '@/utils/clsx';
 import style from './styles.module.css';
-import fontStyle from '../../../../../../styles/font-style.module.css';
 
 export default function PostCardDescription({
   description,
