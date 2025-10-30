7ac44ceedaf877843cfec99315ad38db3fe1c137
André Menezes <1992menezes@gmail.com>
2025-10-30 10:45:19 -0300

    Update imports on HandleParagraph component

--- src/components/ui/container/StructuredTextDatoCMS/handleParagraph/index.tsx
+++ src/components/ui/container/StructuredTextDatoCMS/handleParagraph/index.tsx
@@ -1,5 +1,5 @@
 import { PropsWithChildren } from 'react';
-import fontStyle from '../../../../../../styles/font-style.module.css';
+import fontStyle from '@/../styles/fontStyle.module.css';
 
 export default function HandleParagraph({ children }: PropsWithChildren) {
   return <p className={fontStyle.postBody}>{children}</p>;
