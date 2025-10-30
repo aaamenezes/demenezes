57a0c3713a1b0b46e68c6e86055599dbe2347d1b
André Menezes <1992menezes@gmail.com>
2025-10-30 11:09:50 -0300

    Update imports on generate-sitemap

--- src/scripts/generate-sitemap.ts
+++ src/scripts/generate-sitemap.ts
@@ -1,33 +1,33 @@
-import fs from 'fs'
-import { SitemapItem } from '../types'
+import fs from 'fs';
+import { SitemapItem } from '@/types';
 
-const BASE_URL = 'https://demenezes.dev/'
+const BASE_URL = 'https://demenezes.dev/';
 
 export async function generateSitemap(postsSitemapData: SitemapItem[]) {
-  const allSitemapData = postsSitemapData
-  allSitemapData.push({ slug: '' }) // home
-  allSitemapData.push({ slug: 'contact' })
-  allSitemapData.push({ slug: 'about' })
+  const allSitemapData = postsSitemapData;
+  allSitemapData.push({ slug: '' }); // home
+  allSitemapData.push({ slug: 'contact' });
+  allSitemapData.push({ slug: 'about' });
 
   const sitemapItems = allSitemapData
     .map(({ slug, _updatedAt }) => {
       const updatedAt = _updatedAt
         ? `<lastmod>${_updatedAt}</lastmod>`
         : slug === ''
-          ? `<lastmod>${allSitemapData[0]._updatedAt}</lastmod>`
-          : ''
+        ? `<lastmod>${allSitemapData[0]._updatedAt}</lastmod>`
+        : '';
 
-      return (`  <url>
+      return `  <url>
         <loc>${`${BASE_URL}${slug}`}</loc>
         ${updatedAt}
-      </url>`)
+      </url>`;
     })
-    .join('\n')
+    .join('\n');
 
   const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
 ${sitemapItems}
-</urlset>`
+</urlset>`;
 
-  fs.writeFileSync('public/sitemap.xml', sitemap)
+  fs.writeFileSync('public/sitemap.xml', sitemap);
 }
