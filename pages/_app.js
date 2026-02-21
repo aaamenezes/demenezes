1d7a6574ab5eac368633ae872c40dbd946b0acfe
André Menezes <1992menezes@gmail.com>
2026-02-21 14:26:16 -0300

    Comentários sobre CSSs

--- pages/_app.js
+++ pages/_app.js
@@ -2,9 +2,20 @@ import { useRouter } from 'next/router'
 import { useEffect } from 'react'
 import { pageview } from '../src/external/GoogleAnalytics/gtag.ts'
 import GoogleAnalytics from '../src/external/GoogleAnalytics/index.tsx'
+
+/**
+ * reset.css: corrigir inconsistências entre navegadores e fornecer uma base neutra para estilos personalizados. Pode ser copiado diretamente para outro projeto
+ */
 import '../styles/reset.css'
+
+/**
+ * variables.css: definir variáveis nativas do CSS para cores, tipografia, espaçamento e outros estilos reutilizáveis
+ */
 import '../styles/variables.css'
-import '../styles/reset.css'
+
+/**
+ * globals.css: estilos globais específicos para esse projeto
+ */
 import '../styles/globals.css'
 
 export default function MyApp({ Component, pageProps }) {
