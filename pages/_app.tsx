bd0c80e30449a19b3408d7ed893aab5ff9de74b8
André Menezes <1992menezes@gmail.com>
2026-02-21 14:30:10 -0300

    Renomear _app de js para tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,40 +1,40 @@
-import { useRouter } from 'next/router'
-import { useEffect } from 'react'
-import { pageview } from '../src/external/GoogleAnalytics/gtag.ts'
-import GoogleAnalytics from '../src/external/GoogleAnalytics/index.tsx'
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
+import { pageview } from '../src/external/GoogleAnalytics/gtag';
+import GoogleAnalytics from '../src/external/GoogleAnalytics/index';
 
 /**
  * reset.css: corrigir inconsistências entre navegadores e fornecer uma base neutra para estilos personalizados. Pode ser copiado diretamente para outro projeto
  */
-import '../styles/reset.css'
+import '../styles/reset.css';
 
 /**
  * variables.css: definir variáveis nativas do CSS para cores, tipografia, espaçamento e outros estilos reutilizáveis
  */
-import '../styles/variables.css'
+import '../styles/variables.css';
 
 /**
  * globals.css: estilos globais específicos para esse projeto
  */
-import '../styles/globals.css'
+import '../styles/globals.css';
 
 export default function MyApp({ Component, pageProps }) {
-  const router = useRouter()
+  const router = useRouter();
 
   useEffect(() => {
     const handleRouteChange = url => {
-      pageview(url)
-    }
-    router.events.on('routeChangeComplete', handleRouteChange)
+      pageview(url);
+    };
+    router.events.on('routeChangeComplete', handleRouteChange);
     return () => {
-      router.events.off('routeChangeComplete', handleRouteChange)
-    }
-  }, [router.events])
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [router.events]);
 
   return (
     <>
       <Component {...pageProps} />
       <GoogleAnalytics />
     </>
-  )
+  );
 }
