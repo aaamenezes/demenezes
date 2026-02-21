9bc905b4d2abdf054513d1ea66003c49f35bef4c
André Menezes <1992menezes@gmail.com>
2026-02-21 14:46:20 -0300

    Criar variantes do button

--- src/components/ui/base/Button/index.tsx
+++ src/components/ui/base/Button/index.tsx
@@ -4,6 +4,7 @@ import type { ButtonProps } from './types';
 
 export default function Button({
   children,
+  variant = 'filled',
   onClick = () => {},
   type = 'button',
   disabled = false,
@@ -17,7 +18,7 @@ export default function Button({
   return (
     <button
       onClick={onClick}
-      className={clsx(style.button, className)}
+      className={clsx(style.button, style[`variant-${variant}`], className)}
       type={type}
       disabled={disabled}
       name={name}
