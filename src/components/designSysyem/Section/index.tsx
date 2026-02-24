44c782113560e696968fdba81a270d31bc201c4a
André Menezes <1992menezes@gmail.com>
2026-02-24 08:43:57 -0300

    Dinamize direction of sectionContent in design system page

--- src/components/designSysyem/Section/index.tsx
+++ src/components/designSysyem/Section/index.tsx
@@ -1,17 +1,22 @@
+import { clsx } from '@/utils/clsx';
 import style from './styles.module.css';
 
 export default function Section({
   heading,
+  direction = 'row',
   children,
 }: {
   heading: string;
+  direction: 'row' | 'column';
   children: React.ReactNode;
   styleFromProps?: React.CSSProperties;
 }) {
   return (
     <section className={style.section}>
       <h2 className={style.sectionTitle}>{heading}</h2>
-      <div className={style.sectionContent}>{children}</div>
+      <div className={clsx(style.sectionContent, style[direction])}>
+        {children}
+      </div>
     </section>
   );
 }
