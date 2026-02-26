1b13f58c1e1034e6dd3d1c535a6030c6ed197d95
André Menezes <1992menezes@gmail.com>
2026-02-25 23:03:15 -0300

    Adapt page to new design system page

--- src/components/designSysyem/Page/index.tsx
+++ src/components/designSysyem/Page/index.tsx
@@ -1,5 +1,5 @@
 import style from './styles.module.css';
 
 export default function Page({ children }: { children: React.ReactNode }) {
-  return <div className={style.page}>{children}</div>;
+  return <main className={style.page}>{children}</main>;
 }
