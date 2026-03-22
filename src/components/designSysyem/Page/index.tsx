2eb6cc42489e0dc04dfc52f1535f58241488bfc4
André Menezes <1992menezes@gmail.com>
2026-03-22 12:03:51 -0300

    Remover tags main excedentes

--- src/components/designSysyem/Page/index.tsx
+++ src/components/designSysyem/Page/index.tsx
@@ -1,5 +1,5 @@
 import style from './styles.module.css';
 
 export default function Page({ children }: { children: React.ReactNode }) {
-  return <main className={style.page}>{children}</main>;
+  return <div className={style.page}>{children}</div>;
 }
