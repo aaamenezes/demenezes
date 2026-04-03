333af6310060a2d042cd4b6a167bad9456fcdbea
André Menezes <1992menezes@gmail.com>
2026-04-03 19:56:39 -0300

    Adjust component page content

--- src/components/ui/template/Components/index.tsx
+++ src/components/ui/template/Components/index.tsx
@@ -4,21 +4,25 @@ import Section from '@/components/designSysyem/Section';
 import Sidebar from '@/components/designSysyem/Sidebar';
 import { getComponentByPath } from './getComponentByPath';
 import type { ComponentPathProps } from './types';
+import { capitalize } from '@/utils/capitalize';
 
 export default function ComponentsScreen({
   level,
   componentName,
 }: ComponentPathProps) {
   const Component = getComponentByPath({ level, componentName });
+  const capitalizedComponentName = capitalize(componentName);
+  const capitalizedComponentNameWithBrackets = `<${capitalizedComponentName}>`;
 
   return (
     <Page>
       <Sidebar />
       <Content>
-        <Section heading="Nome do componente" direction="row">
-          <h1>
-            {level} / {componentName}
-          </h1>
+        <Section
+          heading={`${level} / ${capitalizedComponentNameWithBrackets}`}
+          direction="row"
+        >
+          <></>
         </Section>
         <Component />
       </Content>
