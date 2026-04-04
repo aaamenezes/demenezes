b437f2ca69f389208a499b28cf49d8d1cd89cc16
André Menezes <1992menezes@gmail.com>
2026-04-04 19:09:14 -0300

    Style sidebar links

--- src/components/designSysyem/Sidebar/index.tsx
+++ src/components/designSysyem/Sidebar/index.tsx
@@ -1,15 +1,27 @@
+import Link from '@/components/ui/base/Link';
+import { clsx } from '@/utils/clsx';
 import { getDesignSystemValidPaths } from '@/utils/getDesignSystemValidPaths';
+import { useRouter } from 'next/router';
 import Section from '../Section';
 import styles from './styles.module.css';
 
 export default function Sidebar() {
   const designSystemValidPaths = getDesignSystemValidPaths();
+  const router = useRouter();
 
   const sideBarContent = designSystemValidPaths.map(
     ({ level, componentName }) => {
+      const href = `/components/${level}/${componentName}/`;
+      const isComponentActive = router.asPath === href;
+
       return (
         <li key={`${level}-${componentName}`}>
-          <a href={`/components/${level}/${componentName}`}>{componentName}</a>
+          <Link
+            href={href}
+            className={clsx({ [styles.isLinkActive]: isComponentActive })}
+          >
+            {componentName}
+          </Link>
         </li>
       );
     }
@@ -18,7 +30,7 @@ export default function Sidebar() {
   return (
     <aside className={styles.sidebar}>
       <Section heading="Sidebar" direction="column">
-        {sideBarContent}
+        <ul>{sideBarContent}</ul>
       </Section>
     </aside>
   );
