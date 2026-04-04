618f1561d78fb8da9e47cddba56c3709a05f4f0b
André Menezes <1992menezes@gmail.com>
2026-04-04 20:08:31 -0300

    Create hiddenA11Y story

---
+++ src/components/ui/base/HiddenA11Y/story.tsx
@@ -0,0 +1,18 @@
+import Section from '@/components/designSysyem/Section';
+import HiddenA11Y from '.';
+
+export default function HiddenA11YStory() {
+  return (
+    <>
+      <Section
+        heading="Visualmente escondido, mas acessível (usar DevTools para testar)"
+        direction="column"
+      >
+        <HiddenA11Y>
+          Texto a ser escondido visualmente, mas acessível para leitores de
+          tela.
+        </HiddenA11Y>
+      </Section>
+    </>
+  );
+}
--- src/components/ui/template/Components/data.ts
+++ src/components/ui/template/Components/data.ts
@@ -1,11 +1,13 @@
 import ButtonStory from '../../base/Button/story';
 import CodeStory from '../../base/Code/story';
+import HiddenA11YStory from '../../base/HiddenA11Y/story';
 import type { Level, ComponentsMapProps } from './types';
 
 export const componentsByLevelMap: ComponentsMapProps = {
   base: {
     button: ButtonStory,
     code: CodeStory,
+    hiddenA11Y: HiddenA11YStory,
   },
   block: {},
   container: {},
