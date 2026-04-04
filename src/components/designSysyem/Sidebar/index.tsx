855104452711ba3ea7fde5b6be45ac743fd7e9e3
André Menezes <1992menezes@gmail.com>
2026-04-04 14:22:25 -0300

    Create initial content on design system sidebar

--- src/components/designSysyem/Sidebar/index.tsx
+++ src/components/designSysyem/Sidebar/index.tsx
@@ -1,8 +1,25 @@
+import { getDesignSystemValidPaths } from '@/utils/getDesignSystemValidPaths';
+import Section from '../Section';
+import styles from './styles.module.css';
+
 export default function Sidebar() {
+  const designSystemValidPaths = getDesignSystemValidPaths();
+
+  const sideBarContent = designSystemValidPaths.map(
+    ({ level, componentName }) => {
+      return (
+        <li key={`${level}-${componentName}`}>
+          <a href={`/components/${level}/${componentName}`}>{componentName}</a>
+        </li>
+      );
+    }
+  );
+
   return (
-    <aside>
-      <h2>Sidebar</h2>
-      <p>This is the sidebar component.</p>
+    <aside className={styles.sidebar}>
+      <Section heading="Sidebar" direction="column">
+        {sideBarContent}
+      </Section>
     </aside>
   );
 }
