81f221688740d5f55c38576ffbb3b17a511a4d45
André Menezes <1992menezes@gmail.com>
2026-07-28 21:51:43 -0300

    Update wrapper component story

--- src/components/ui/container/Wrapper/story.tsx
+++ src/components/ui/container/Wrapper/story.tsx
@@ -4,27 +4,87 @@ import Wrapper from '.';
 export default function WrapperStory() {
   return (
     <>
-      <Section heading="Default">
-        <Wrapper width="md">
-          <p>Wrapper com largura limitada padrão.</p>
+      <Section heading="Small width">
+        <Wrapper width="sm">
+          <p>
+            Wrapper com largura sm. Lorem ipsum dolor sit amet consectetur
+            adipisicing elit. Laborum mollitia, at magni sequi similique iusto
+            quis non eos amet quisquam pariatur! Voluptas blanditiis rem, quod
+            non impedit libero assumenda cumque?
+          </p>
         </Wrapper>
       </Section>
 
-      <Section heading="Fluid">
-        <Wrapper width="md" isFluid>
-          <p>Wrapper fluido dentro do breakpoint escolhido.</p>
+      <Section heading="Medium width">
+        <Wrapper width="md">
+          <p>
+            Wrapper com largura md. Lorem ipsum dolor sit amet consectetur
+            adipisicing elit. Laborum mollitia, at magni sequi similique iusto
+            quis non eos amet quisquam pariatur! Voluptas blanditiis rem, quod
+            non impedit libero assumenda cumque?
+          </p>
+        </Wrapper>
+      </Section>
+
+      <Section heading="Large width">
+        <Wrapper width="lg">
+          <p>
+            Wrapper com largura lg. Lorem ipsum dolor sit amet consectetur
+            adipisicing elit. Laborum mollitia, at magni sequi similique iusto
+            quis non eos amet quisquam pariatur! Voluptas blanditiis rem, quod
+            non impedit libero assumenda cumque?
+          </p>
+        </Wrapper>
+      </Section>
+
+      <Section heading="Extra large width">
+        <Wrapper width="xl">
+          <p>
+            Wrapper com largura xl. Lorem ipsum dolor sit amet consectetur
+            adipisicing elit. Laborum mollitia, at magni sequi similique iusto
+            quis non eos amet quisquam pariatur! Voluptas blanditiis rem, quod
+            non impedit libero assumenda cumque?
+          </p>
+        </Wrapper>
+      </Section>
+
+      <Section heading="Double extra large width">
+        <Wrapper width="xxl">
+          <p>
+            Wrapper com largura xxl. Lorem ipsum dolor sit amet consectetur
+            adipisicing elit. Laborum mollitia, at magni sequi similique iusto
+            quis non eos amet quisquam pariatur! Voluptas blanditiis rem, quod
+            non impedit libero assumenda cumque?
+          </p>
+        </Wrapper>
+      </Section>
+
+      <Section heading="Triple extra large width">
+        <Wrapper width="xxxl">
+          <p>
+            Wrapper com largura xxxl. Lorem ipsum dolor sit amet consectetur
+            adipisicing elit. Laborum mollitia, at magni sequi similique iusto
+            quis non eos amet quisquam pariatur! Voluptas blanditiis rem, quod
+            non impedit libero assumenda cumque?
+          </p>
         </Wrapper>
       </Section>
 
       <Section heading="Full width">
         <Wrapper width="full">
-          <p>Wrapper ocupando 100% da largura disponível.</p>
-        </Wrapper>
-      </Section>
-
-      <Section heading="Spacing">
-        <Wrapper width="lg" spacing={2}>
-          <p>Wrapper com margin-bottom configurada via spacing.</p>
+          <p>
+            Wrapper ocupando 100% da largura disponível. Lorem ipsum dolor sit
+            amet consectetur adipisicing elit. Laborum mollitia, at magni sequi
+            similique iusto quis non eos amet quisquam pariatur! Voluptas
+            blanditiis rem, quod non impedit libero assumenda cumque? Laborum
+            mollitia, at magni sequi similique iusto quis non eos amet quisquam
+            pariatur! Voluptas blanditiis rem, quod non impedit libero assumenda
+            cumque? Laborum mollitia, at magni sequi similique iusto quis non
+            eos amet quisquam pariatur! Voluptas blanditiis rem, quod non
+            impedit libero assumenda cumque? Laborum mollitia, at magni sequi
+            similique iusto quis non eos amet quisquam pariatur! Voluptas
+            blanditiis rem, quod non impedit libero assumenda cumque?
+          </p>
         </Wrapper>
       </Section>
     </>
