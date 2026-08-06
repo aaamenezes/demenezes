0723930ec85191c446f7189d2333f53e44aacce5
André Menezes <1992menezes@gmail.com>
2026-08-06 10:49:08 -0300

    feat: add isColorInverse prop to Logo component for customizable color styling

--- src/components/ui/block/Logo/index.tsx
+++ src/components/ui/block/Logo/index.tsx
@@ -1,12 +1,20 @@
 import Link from '@/components/ui/base/Link';
 import style from './styles.module.css';
+import { clsx } from '@/utils/clsx';
 
-export default function Logo() {
+export default function Logo({
+  isColorInverse = false,
+}: {
+  isColorInverse?: boolean;
+}) {
   // quando reativar o preview, resolver isso
   const preview = false;
 
   return (
-    <Link className={style.logoLink} href="/">
+    <Link
+      className={clsx(style.logoLink, { [style.colorInverse]: isColorInverse })}
+      href="/"
+    >
       {preview ? 'Preview' : 'deMenezes'}
     </Link>
   );
