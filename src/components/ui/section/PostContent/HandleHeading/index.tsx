e3060d2482e8b7e63cc6163663eb0cb4e1ae487e
André Menezes <1992menezes@gmail.com>
2026-08-16 11:49:26 -0300

    feat(HandleHeading): enhance component to accept children prop for dynamic heading content

--- src/components/ui/section/PostContent/HandleHeading/index.tsx
+++ src/components/ui/section/PostContent/HandleHeading/index.tsx
@@ -2,28 +2,43 @@ import { clsx } from '@/utils/clsx';
 import { simplifyString } from '@/utils/simplifyString';
 import type { Heading as HeadingNode } from 'datocms-structured-text-utils';
 import { isSpan } from 'datocms-structured-text-utils';
+import type { ReactNode } from 'react';
 import style from '../styles.module.css';
 
-export default function HandleHeading({ node }: { node: HeadingNode }) {
+export default function HandleHeading({
+  node,
+  children,
+}: {
+  node: HeadingNode;
+  children: ReactNode;
+}) {
   const HeadingLevel = `h${node.level}` as keyof HTMLElementTagNameMap;
 
-  const headingText = node.children
-    .filter(isSpan)
-    .map(child => child.value)
-    .join('');
-  const id = simplifyString(headingText);
+  const anchorLink = simplifyString(
+    node.children
+      .filter(isSpan)
+      .map(child => child.value)
+      .join('')
+  );
 
-  const headingLevelClassName =
-    style[HeadingLevel as keyof typeof style] || style.h2;
+  function isValidClassName(
+    className: string
+  ): className is keyof typeof style {
+    return className in style;
+  }
+
+  const headingLevelClassName = isValidClassName(HeadingLevel)
+    ? style[HeadingLevel]
+    : style.h2;
 
   return (
     <HeadingLevel
       className={clsx(style.heading, headingLevelClassName)}
-      key={headingText}
-      id={id}
+      key={anchorLink}
+      id={anchorLink}
     >
-      <a className={style.headingLink} href={`#${id}`}>
-        <span className={style.headingLinkText}>{headingText}</span>
+      <a className={style.headingLink} href={`#${anchorLink}`}>
+        <span className={style.headingLinkText}>{children}</span>
       </a>
     </HeadingLevel>
   );
