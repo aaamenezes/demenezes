af2a82bde0589a40321b2a9d8c8e3bfb3d0de619
André Menezes <1992menezes@gmail.com>
2026-08-19 10:38:59 -0300

    feat(Text): enhance Text component with customizable styles for family, size, weight, letterSpacing, and lineHeight

--- src/components/ui/base/Text/index.tsx
+++ src/components/ui/base/Text/index.tsx
@@ -1,13 +1,30 @@
 import type { TextProps } from './types';
+import styles from './styles.module.css';
+import { clsx } from '@/utils/clsx';
 
 export default function Text({
   children,
+  family = 'body',
+  size = 'xs',
+  weight = 400,
+  letterSpacing = 'xs',
+  lineHeight = 'xs',
   as: Tag = 'p',
   className,
   ariaHidden,
 }: TextProps) {
   return (
-    <Tag className={className} aria-hidden={ariaHidden}>
+    <Tag
+      className={clsx(
+        className || '',
+        styles[`font-family-${family}`],
+        styles[`font-size-${size}`],
+        styles[`font-weight-${weight}`],
+        styles[`letter-spacing-${letterSpacing}`],
+        styles[`line-height-${lineHeight}`]
+      )}
+      aria-hidden={ariaHidden}
+    >
       {children}
     </Tag>
   );
--- src/components/ui/base/Text/types.ts
+++ src/components/ui/base/Text/types.ts
@@ -1,7 +1,49 @@
 import type { HTMLAttributes, ReactNode } from 'react';
 
+type FontSize =
+  | 'xxxs'
+  | 'xxs'
+  | 'xs'
+  | 'sm'
+  | 'md'
+  | 'lg'
+  | 'xl'
+  | 'xxl'
+  | 'xxxl';
+
+type FontWeight = 100 | 200 | 300 | 400 | 500 | 600 | 700 | 800 | 900;
+
+type LetterSpacing =
+  | 'xxxs'
+  | 'xxs'
+  | 'xs'
+  | 'sm'
+  | 'md'
+  | 'lg'
+  | 'xl'
+  | 'xxl'
+  | 'xxxl';
+
+type LineHeight =
+  | 'xxxs'
+  | 'xxs'
+  | 'xs'
+  | 'sm'
+  | 'md'
+  | 'lg'
+  | 'xl'
+  | 'xxl'
+  | 'xxxl';
+
+type FontFamily = 'body' | 'heading' | 'monospace';
+
 export interface TextProps extends HTMLAttributes<HTMLElement> {
   children: ReactNode;
+  family?: FontFamily;
+  size?: FontSize;
+  weight?: FontWeight;
+  letterSpacing?: LetterSpacing;
+  lineHeight?: LineHeight;
   as?: 'p' | 'span' | 'strong';
   className?: string;
   ariaHidden?: boolean;
