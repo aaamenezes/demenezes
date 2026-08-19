24a2b3e9d73e8d0806d7344a0d8583a80d4e964f
André Menezes <1992menezes@gmail.com>
2026-08-19 19:43:20 -0300

    Implement Heading component on AboutHobbies

--- src/components/ui/section/AboutHobbies/index.tsx
+++ src/components/ui/section/AboutHobbies/index.tsx
@@ -1,3 +1,4 @@
+import Heading from '@/components/ui/base/Heading';
 import Text from '@/components/ui/base/Text';
 import Wrapper from '@/components/ui/container/Wrapper';
 import style from './styles.module.css';
@@ -11,7 +12,7 @@ export default function AboutHobbies({
 }) {
   return (
     <Wrapper as="section" width="md">
-      <h2 className={style.hobbiesTitle}>{hobbiesTitle}</h2>
+      <Heading className={style.hobbiesTitle}>{hobbiesTitle}</Heading>
       <Text lineHeight="md">{hobbiesText}</Text>
     </Wrapper>
   );
